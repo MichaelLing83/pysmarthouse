0ee878f3f0e0075f14dc371d490625d61f116e68
Michael Duo Ling <duo.ling.cn@gmail.com>
2014-11-30 16:43:24 +0100

    Fix: remove redundant code of RbgLight example.

--- RbgLight/RbgLight.ino
+++ RbgLight/RbgLight.ino
@@ -10,16 +10,6 @@ Connection:
 #include "RbgLight.h"
 
 RbgLight rbgLight(8, 9, 10, HIGH);
-#define R_PIN 8
-#define B_PIN 9
-#define G_PIN 10
-#define LIGHT_ON HIGH
-#define LIGHT_OFF LOW
-
-// color map: R, B, G (bit 0, bit 1, bit 2)
-const unsigned int RED = 1;
-const unsigned int BLUE = 2;
-const unsigned int GREEN = 4;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -30,36 +20,5 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
     rbgLight.on(random(0, 8));
-    //light_on(random(0, 8));
     delay(500);
-}
-
-void light_on(unsigned int color)
-{
-    if (color & RED)
-    {
-        digitalWrite(R_PIN, LIGHT_ON);
-    } else {
-        digitalWrite(R_PIN, LIGHT_OFF);
-    }
-
-    if (color & BLUE)
-    {
-        digitalWrite(B_PIN, LIGHT_ON);
-    } else {
-        digitalWrite(B_PIN, LIGHT_OFF);
-    }
-
-    if (color & GREEN)
-    {
-        digitalWrite(G_PIN, LIGHT_ON);
-    } else {
-        digitalWrite(G_PIN, LIGHT_OFF);
-    }
-}
-
-void light_off() {
-    digitalWrite(R_PIN, LIGHT_OFF);
-    digitalWrite(B_PIN, LIGHT_OFF);
-    digitalWrite(G_PIN, LIGHT_OFF);
 }
