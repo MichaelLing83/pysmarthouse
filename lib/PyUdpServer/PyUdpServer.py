6db50f41b5b56774294947a76dd90428224b88bd
Michael Duo Ling <duo.ling.cn@gmail.com>
2014-12-06 20:09:13 +0100

    ADD: PyUdpServer module

---
+++ lib/PyUdpServer/PyUdpServer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import socketserver
+
+class MyUDPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print("{} wrote:".format(self.client_address[0]))
+        print(data)
+        socket.sendto(data.upper(), self.client_address)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+    server = socketserver.UDPServer((HOST, PORT), MyUDPHandler)
+    server.serve_forever()
