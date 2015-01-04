4d7dc21989f948f802cb5a83eaca34271152cc81
Michael Duo Ling <duo.ling.cn@gmail.com>
2015-01-04 12:19:23 +0100

    send ok

--- ArduinoNano/ArduinoNano.ino
+++ ArduinoNano/ArduinoNano.ino
@@ -10,16 +10,46 @@ void setup()
 {
     Serial.begin(9600);
     esp8266.begin(9600);
+    sendDebug("AT+RST");    // reset
+    delay(2000);
     sendDebug("AT");
+    sendDebug("ATE0");  // disable echo
+    connectWiFi();
+    sendDebug("AT+CIPSTATUS");
+    
+    sendDebug("AT+CIPSTART=\"UDP\",\"192.168.31.107\",9999");
 }
 
 void loop()
 {
+    //char cmd[30] = "Kitchen;Temperature;-1900";
+    String data = "Kitchen;Temperature;-1900";
+    String cmd = "AT+CIPSEND=";
+    cmd += data.length();
+    esp8266.println(cmd);
+    Serial.println(cmd);
+    if (esp8266.find(">")) {
+        Serial.print(">");
+        esp8266.print(data);
+        Serial.println(data);
+    } else {
+        sendDebug("AT+CIPCLOSE");
+    }
+    delay(2000);
 }
 
 void sendDebug(String cmd){
     esp8266.println(cmd);
     Serial.println(cmd);
+    delay(1000);
+    char tmp;
+    tmp = esp8266.read();
+    Serial.print(">");
+    while (tmp != -1) {
+        Serial.print(tmp);
+        tmp = esp8266.read();
+    }
+    Serial.print("\n");
 } 
 
 boolean connectWiFi()
