6b3e7116f0e3eea74726ac2350cb001c058f5c36
Michael Duo Ling <duo.ling.cn@gmail.com>
2015-01-05 15:55:03 +0100

    Add submodule for ESP8266. Update ArduinoController to use ESP8266 module.

--- ArduinoController/ArduinoController.ino
+++ ArduinoController/ArduinoController.ino
@@ -13,22 +13,37 @@
         5. one ESP8266 (Serial over Wifi);
 
     Connection:
-        # relay
-            VCC ->  
+        1 relay
+            VCC ->
             GND -> GND
             IN  -> PIN_3
-        # DS18B20
+        2 DS18B20
             GND ->  GND
             VDD ->  5V
             DQ  ->  4.7 kOhm    ->  5V
-        # light sensor
+        3 light sensor
             VCC ->  5V
             GND ->  GND
             DO  ->  PIN_5
-        # Infrared sensor
+        4 Infrared sensor
             VCC ->  5V
             GND ->  GND
             OUT ->  PIN_6
+
+        5 ESP8266
+            If you hold ESP8266 with antenna end upwards and all chips facing you, then from upper left corner to upper right corner, then
+            from lower left corner to lower right corner, you have pins:
+                GND,    GPIO2,  GPIO0,  URXD
+                UTXD,   CH_PD,  RST,    VCC
+            connection:
+                GND     ->  GND
+                URXD    ->  TXD 1
+                UTXD    ->  RXD 0
+                CH_PD   ->  3.3V
+                VCC     ->  3.3V
+            connection for debugging:
+                URXD    ->  PIN_11
+                UTXD    ->  PIN_10
 
     Overall description:
         1. Use ESP8266 to connect to house's wifi and then to Raspberry Pi;
@@ -42,6 +57,8 @@
 #include "TemperatureSensor.h"
 #include "LightSensor.h"
 #include "InfraredSensor.h"
+#include "uartWIFI.h"
+#include <SoftwareSerial.h>
 
 /**** Defines ****************************************************************/
 // Relay related
@@ -53,6 +70,14 @@
 #define LIGHTSENSOR_PORT 5
 // Infrared sensor
 #define INFRAREDSENSOR_PORT 6
+// WIFI config for ESP8266
+#define SSID       "michael"
+#define PASSWORD   "waterpigs"
+#define SERVER_IP "192.168.31.107" //RaspberryPi
+#define SERVER_PORT 9999
+// Reporting configuration
+#define REPORT_INTERVAL 60  // in seconds, between two reports
+#define ARDUINO_ID "FrontDoorStep"  // ID
 /**** End of Defines *********************************************************/
 
 /**** Global Variables *******************************************************/
@@ -60,6 +85,8 @@ Relay relay(RELAY_IN_PORT, RELAY_OFF);
 OneWire ds18b20(DS18B20_PIN);
 LightSensor lightSensor(LIGHTSENSOR_PORT);
 InfraredSensor infraredSensor(INFRAREDSENSOR_PORT);
+WIFI wifi;  // ESP8266 serial over Wifi
+char wifiBuffer[128];   // buffer for receiving data
 
 unsigned long last_report_time;
 /**** End of Global Variables ************************************************/
@@ -76,18 +103,31 @@ void setup() {
     relay.begin(RELAY_OFF);
     lightSensor.begin();
     infraredSensor.begin();
-    
+
     // register all bitlash functions
     register_bitlash_relay();
     register_bitlash_ds18b20();
     register_bitlash_infraredSensor();
-    
+
     last_report_time = 0;
+
+    // initialize ESP8266
+    wifi.begin();
+    if(!wifi.Initialize(STA, SSID, PASSWORD)) {
+        wifi.Initialize(STA, SSID, PASSWORD);
+    }
+    delay(8000);  //make sure the module have enough time to get an IP address
+    //DebugSerial.println(wifi.showIP()); //show ip address of module
+
+    //delay(5000);
+    wifi.ipConfig(UDP, SERVER_IP, SERVER_PORT); // configure server info
+
+    //DebugSerial.println("setup done..");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    // we send a report every 5 minutes
+    // we send a report every 60 seconds
     unsigned long cur_time = millis();
     bool do_report = false;
     if (cur_time < last_report_time)
@@ -95,7 +135,7 @@ void loop() {
         // time counter has wrapped around.
         do_report = true;
     }
-    else if (cur_time - last_report_time >= 5*60*1000)
+    else if (cur_time - last_report_time >= REPORT_INTERVAL*1000)
     {
         do_report = true;
     }
@@ -103,6 +143,44 @@ void loop() {
     if (do_report)
     {
         last_report_time = cur_time;
-        float temperature = get_temperature(&ds18b20);
+        String report = String("REPORT;"); // + String(ARDUINO_ID) + ";";
+        report += ARDUINO_ID;
+        report += ";";
+        // relay status
+        report += "Relay;";
+        report += String(int(100 * relay.cur_lvl()));
+        report += ";";
+        // temperature sensor
+        report += "Temperature;";
+        report += String(int(100 * get_temperature(&ds18b20)));
+        report += ";";
+        // light sensor
+        report += "Light;";
+        report += String(int(100 * lightSensor.get()));
+        report += ";";
+        // infra-red sensor
+        report += String("Infrared;");
+        report += String(int(100 * infraredSensor.get()));
+        report += ";";
+        wifi.Send(report);
+        delay(2000);    // do we need to be sure sending is finished?
+
+        // receive response (cmd) from server
+        int data_len = wifi.ReceiveMessage(wifiBuffer);
+        if (data_len >= 4) {    // minimum length of CMD datagram, i.e. "CMD;"
+            String datagram = String(wifiBuffer);
+            if (datagram.startsWith("CMD;", 0)) {
+                // this is a valid CMD datagram
+                datagram = datagram.substring(4);
+                while (datagram.length() > 0) {
+                    // there is at least one cmd to be executed
+                    int index = datagram.indexOf(';');
+                    String cmd = datagram.substring(0, index);
+                    cmd.toCharArray(wifiBuffer, cmd.length());
+                    doCommand(wifiBuffer);
+                    datagram = datagram.substring(index+1);
+                }
+            }
+        }
     }
 }
