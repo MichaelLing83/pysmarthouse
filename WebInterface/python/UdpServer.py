1da43a2cfa9ffbd0c6c9a61ecf4fdc022b9848af
Michael Duo Ling <duo.ling.cn@gmail.com>
2015-01-05 12:05:47 +0100

    Update communication protocol between Arduino clients and Raspberry Pi server.

--- WebInterface/python/UdpServer.py
+++ WebInterface/python/UdpServer.py
@@ -5,13 +5,32 @@ UdpServer class that receives all packets from Arduino's and update database.
 All clients have to follow this format when sending to this server:
     1. Use UDP
     2. Port 9999
-    3. each payload format (as a string coded in ASCII):
-        ID;type;value
-            ID: identity of the sender, e.g. "Kitchen"
-            type: type of the report, e.g. "Temperature", "Relay"
-            value: integer in unit of 0.001
-        Exemples:
-            "Kitchen;Temperature;-1900"    ->  Kitchen Arduino sent us with temperature of -19.00 degree Celsius
+    3. Communication protocol (version 0.1):
+        1) syntax (all letters are case sensitive)
+            All fields are separated by a ";", and usage of ";" within any field is forbidden. After the last field, there is no ";". Each
+            field is a ASCII string. The first field is <operation>, which must be one of "REPORT" or "CMD".
+            When <operation>="REPORT", the datagram is composed of 1+1+2*n fields in strict order of "REPORT", <ID>, (<type>, <value>)+:
+                <ID>: identity of the client, usually a geometry location name of an Arduino, e.g. "KitchenWindow", "FrontYard"
+                <type>: type of report, i.e. what the <value> field in this report refers to, e.g. "Temperature", "InternetConnectivity"
+                <value>: integer in unit of 0.001, e.g. "-1800" means -18.00
+                Example: "REPORT;KitchenWindow;temperature;2300;relay;100", means the sender "KitchenWindow" reports a temperature at 23.00
+                degree Celsius and its relay at value 1.00 (ON).
+            When <operation>="CMD", the datagram is composed of 1+1*n fields in strict order of "CMD", (<cmd>)+:
+                <cmd>: the command receiver should execute.
+                Example: "CMD;relay_on();delay(1000);relay_off()", means after receiving this datagram the receiver should execute
+                "relay_on()" and then "delay(1000)" and then "relay_off()".
+        2) semantics
+            client -> server: only "REPORT" operation is supported, e.g. "REPORT:Doorstep;Temperature;-1900", means client named "Doorstep"
+            reports a temperature at -19.00 degree Celsius. How server uses the content of this report is completely up to the server.
+            server -> client: only "CMD" operation is supported, e.g. "CMD:reset".
+        3) timing
+            A TIMEOUT of 4 seconds is applied to each receiving attempt, if no response is received before timeout, operator should just
+            reset its status and try again later (ideally a counter of consecutive timeout should also be maintained).
+            Server should always listen, and any operation to handle a incoming datagram should take no more than TIMEOUT/2 in time. Server
+            only responses to incoming datagram and then sends a datagram as response.
+            Client sends one REPORT datagram to server every PERIOD (=60 second) or when an significant event happened, e.g. LightSensor
+            threshold is reached, etc. When client sends a report, it waits for an response which is a "CMD" datagram, and executes <cmd>
+            in the datagram.
 '''
 
 import db
